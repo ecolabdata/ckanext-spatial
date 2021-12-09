acaf0eb0cf5019599e1337604f8bcb3d2914acd4
alhyss <55992003+alhyss@users.noreply.github.com>
2021-12-09 21:38:57 +0100

    Update ogcfilter.py
    Nouvelle approche pour la dé-sérialisation des conditions élémentaires par `as_owslib_expression` : beaucoup plus robuste et permet d'exploiter toutes les possibilités offertes par les filtres d'OWSLib. 
    
    À l'inverse, `parse_constraint` est ré-écrite sous une forme plus limitative : plus d'appels récursifs qui auraient permis de prendre en charge des structures complexes... si seulement OWSLib avait su les gérer.
    
    Classes dédiées pour les erreurs.
    
    Amélioration des docstrings.

--- ckanext/spatial/lib/ogcfilter.py
+++ ckanext/spatial/lib/ogcfilter.py
@@ -1,93 +1,15 @@
-"""
-Small functions to handle OGC filters.
+"""OGC filters handling.
 
-Syntax is similar to OWSLib's classes, albeit suited
-to JSON encoding.
-"""
+This module provides a function, :py:func:`parse_constraints`,
+to parse the parameter `ogcfilter` of the harvest source configuration
+into some valid constraints list that can be passed as `constraints`
+parameter of OWSLib's functions such as `getrecords2`.
 
-import owslib.fes as ows
+Syntax of `ogcfilter` is similar to OWSLib's OgcExpression classes,
+albeit suited to JSON encoding.
 
+For example, this JSON configuration::
 
-# OWSLib classes for OGC Filter operators
-# with list of allowed types for each parameter
-ogc_filter_operators = {
-    'PropertyIsLike': [[str], [str]],
-    'PropertyIsNull': [[str]],
-    'PropertyIsBetween': [[str], [str, int, float], [str, int, float]],
-    'PropertyIsGreaterThanOrEqualTo': [[str], [str, int, float]],
-    'PropertyIsLessThanOrEqualTo': [[str], [str, int, float]],
-    'PropertyIsGreaterThan': [[str], [str, int, float]],
-    'PropertyIsLessThan': [[str], [str, int, float]],
-    'PropertyIsNotEqualTo': [[str], [str, int, float]],
-    'PropertyIsEqualTo': [[str], [str, int, float]],
-    'BBox': [[int, float], [int, float], [int, float], [int, float]]
-    }
-
-
-def as_owslib_constraint(constraint):
-    """
-    Rewrite an individual constraint using OWSLib classes (PropertyIsLike...)
-
-    Parameter
-    ---------
-    - constraint : a list, such as
-    ["PropertyIsLike", "MyProperty1", "MyValue1"]
-    
-    First element should be the name of an OGC Filter operator,
-    other elements are parameters for the OWSLib constructor.
-    
-    To negate the operator, add "Not" as first element :
-    ["Not", "PropertyIsLike", "MyProperty1", "MyValue1"]
-    
-    """
-    neg = (constraint[0].upper() == 'NOT')
-
-    if neg:
-        constraint = constraint[1:]
-
-    if not constraint[0] in ogc_filter_operators:
-        raise ValueError("Unknow operator '{}' in ogcfilter".format(constraint[0]))
-
-    if len(constraint[1:]) < len(ogc_filter_operators[constraint[0]]):
-        raise ValueError("Missing some parameters for operator '{}' in ogcfilter".format(constraint[0]))
-
-    if len(constraint[1:]) > len(ogc_filter_operators[constraint[0]]):
-        raise ValueError("Too much parameters for operator '{}' in ogcfilter".format(constraint[0]))
-    
-    if not all(
-        map(
-            lambda x, s: any(
-                map(
-                    lambda t: isinstance(x, t),
-                    s
-                    )
-                ),
-            constraint[1:],
-            ogc_filter_operators[constraint[0]]
-            )
-        ):
-        raise TypeError("At least one parameter of incorrect type for operator '{}' in ogcfilter".format(constraint[0]))
-    
-    s = "ows.{}([{}])" if constraint[0]=='BBox' else "ows.{}({})"
-    # BBox is expecting a list of numbers
-    
-    if neg:
-        s = "ows.Not([{}])".format(s)
-
-    return eval(s.format(constraint[0],", ".join([repr(e) for e in constraint[1:]])))
-
-
-
-def parse_constraints(ogcfilter, rec=None):
-    """
-    Make a valid constraints list out of user-defined ogcfilter parameter
-
-    Parameters
-    ---------
-    - ogcfilter : a list retrieved from user configuration
-    - rec is intended for recursive execution only
-
-    For example, this json configuration
     {
         "ogcfilter": [
             ["PropertyIsLike", "MyProperty1", "MyValue1"],
@@ -95,30 +17,216 @@ def parse_constraints(ogcfilter, rec=None):
         ]
     }
 
-    would be interpreted as
+Will be interpreted as this OWSLib constraints list::
+
     [
-        PropertyIsLike("MyProperty1", "MyValue1"),
-        Not( [ PropertyIsEqualTo("MyProperty2", "MyValue2") ] )
+        PropertyIsLike('MyProperty1', 'MyValue1'),
+        Not([PropertyIsEqualTo('MyProperty2', 'MyValue2')])
     ]
 
-    Result is intended for use as "constraints" parameter of OWSLib's
-    functions such as getrecords2.
+OWSLib constraints lists are fully expanded and `ogcfilter` values
+should be as well. Elements of the list are combined with ``OR``.
+Each element can be an elementary condition (such as 
+``PropertyIsLike("MyProperty1", "MyValue1")`` in OWSLib encoding
+or ``["PropertyIsLike", "MyProperty1", "MyValue1"]`` in JSON
+encoding) or a list of elementary conditions to combine with ``AND``.
+
+Therefore:
+
+    * ``[A]`` is a single elementary condition.
+    * ``[A, B, C]`` equates to ``A`` or ``B`` or ``C``.
+    * ``[[A, B, C]]`` equates to ``A`` and ``B`` and ``C``.
+    * ``[A, [B, C], D]`` equates to ``A`` or ``B`` and ``C`` or ``D``.
+
+Elementary conditions are encoded in JSON as lists.
+
+The first element should be the name of a known OGC Filter operator,
+listed in :py:data:`ogc_filter_operators` (case sensitive) :
+``"PropertyIsLike"`` or ``"PropertyIsEqualTo"``or ``"BBox"``, *etc*.
+
+Following elements are positional mandatory parameters for the
+OWSLib operator, ordered and typed as expected by said operator::
+
+    ["PropertyIsLike", "PropertyName", "Value"]
+
+Last element may be a dict of named optional parameters for the
+operator, such as::
+
+    ["PropertyIsLike", "PropertyName", "Value", {"matchCase": false}]
+
+To negate the operator, add ``'Not'`` as first element of the list::
+
+    ["Not", "PropertyIsLike", "PropertyName", "Value"]
+
+References
+----------
+Documentation of OWSLib `fes` module (on GitHub_). 
+
+.. _GitHub: https://github.com/geopython/OWSLib/blob/master/owslib/fes.py
+
+"""
+
+from owslib.fes import PropertyIsLike, PropertyIsNull, \
+    PropertyIsBetween, PropertyIsGreaterThanOrEqualTo,  \
+    PropertyIsLessThanOrEqualTo, PropertyIsGreaterThan, \
+    PropertyIsLessThan, PropertyIsNotEqualTo, \
+    PropertyIsEqualTo, BBox, Not
+
+ogc_filter_operators = [PropertyIsLike, PropertyIsNull, PropertyIsBetween,
+    PropertyIsGreaterThanOrEqualTo, PropertyIsLessThanOrEqualTo, PropertyIsGreaterThan,
+    PropertyIsLessThan, PropertyIsNotEqualTo, PropertyIsEqualTo, BBox]
+"""List of OWSLib classes for OGC Filter operators.
+
+"""
+
+def as_owslib_expression(constraint):
+    """Rewrite some elementary condition using OWSLib classes.
+
+    Parameters
+    ----------
+    constraint : list
+        A list representing some elementary constraint, such as::
+            ['PropertyIsLike', 'PropertyName', 'Value']
+    
+    Returns
+    -------
+    owslib.fes.OgcExpression
+        A valid elementary condition.
+    
+    Raises
+    ------
+    OgcFilterSyntaxError
+        When `constraint` first element (or second if ``'Not'``
+        is first) is not one of :py:data:`ogc_filter_operators`.
+    OgcFilterParsingError
+        When initialization of the OWSLib object fails.
     
     """
-
-    if rec is None:
-        rec = []
-        
-    for i in range(len(ogcfilter)):
-        
-        if not isinstance(ogcfilter[i], list):
-            raise TypeError("Malformed ogcfilter, expecting a list of lists")
-
-        if isinstance(ogcfilter[i][0], str):        
-            rec.append(as_owslib_constraint(ogcfilter[i]))
-        else:
-            rec.append([])            
-            parse_constraints(ogcfilter[i], rec[i]) 
-        
-    return rec
+    if not constraint:
+        return  
     
+    args = constraint.copy()
+    
+    neg = (args[0].lower() == 'not')
+    if neg:
+        del args[0]
+
+    operator = {o.__name__: o for o in ogc_filter_operators}.get(args[0])
+    if not operator:
+        raise OgcFilterSyntaxError(constraint,
+            detail="Unknow operator '{}'.".format(args[0]))
+    del args[0]
+
+    kwargs = {}
+    if isinstance(args[len(args) - 1], dict):
+        # optional parameters as dict
+        kwargs = args.pop()
+
+    try:
+        res = operator.__call__(*args, **kwargs)
+    except Exception as err:
+        raise OgcFilterParsingError(constraint, err)
+    
+    if neg:
+        res = Not([res])
+
+    return res
+
+
+def parse_constraints(ogcfilter):
+    """Make a valid constraints list out of user-defined ogcfilter parameter.
+
+    Result is intended for use as `constraints` parameter of
+    OWSLib's functions such as `getrecords2`.
+
+    Parameters
+    ----------
+    ogcfilter : list
+        Parameter `ogcfilter` retrieved from user configuration.
+
+    Raises
+    ------
+    OgcFilterSyntaxError
+        If `ogcfilter` is not a list of elementary conditions
+        or a list of lists of elementary conditions. 
+    
+    """
+    if not isinstance(ogcfilter, list):
+        raise OgcFilterSyntaxError(ogcfilter)
+    
+    ors = []
+    
+    for e in ogcfilter:
+        if not isinstance(e, list):
+            raise OgcFilterSyntaxError(ogcfilter)
+        if isinstance(e[0], str):
+            ors.append(as_owslib_expression(e))
+        elif isinstance(e[0], list):
+            ands = []
+            for ee in e:
+                if not isinstance(ee, list) or not isinstance(ee[0], str):
+                    raise OgcFilterSyntaxError(ogcfilter)
+                ands.append(as_owslib_expression(ee))
+            ors.append(ands)
+        else:
+            raise OgcFilterSyntaxError(ogcfilter)
+    
+    return ors
+    
+
+class OgcFilterParsingError(Exception):
+    """When some OWSLib call raises an error.
+    
+    Parameters
+    ----------
+    error : Exception
+        Error raised by some OWSLib function.
+    constraint : list
+        Filter or part of filter for which the error was raised.
+    
+    Attributes
+    ----------
+    error : Exception
+        Error raised by some OWSLib function.
+    constraint : list
+        Filter or part of filter for which the error was raised.
+    
+    """
+    def __init__(self, constraint, error):
+        self.error = error
+        self.constraint = constraint
+    
+    def __str__(self):
+        return "Couldn't parse filter element '{}'. " \
+            "Raised {}.".format(self.constraint, self.error)
+
+
+class OgcFilterSyntaxError(Exception):
+    """When a filter is detected as malformed before any call to OWSLib.
+    
+    Parameters
+    ----------
+    constraint : list
+        Filter or part of filter for which the
+        exception was raised.
+    detail : str, optional
+        Specific message about the error.
+    
+    Attributes
+    ----------
+    constraint : list
+        Filter or part of filter for which the
+        exception was raised.
+    detail : str
+        Specific message about the error.
+    
+    """
+    def __init__(self, constraint, detail=None):
+        self.detail = detail
+        self.constraint = constraint
+    
+    def __str__(self):
+        detail = ' {}'.format(self.detail) if self.detail else ''
+        return "Filter element '{}' is invalid.{}" \
+            ''.format(self.constraint, detail)
+
